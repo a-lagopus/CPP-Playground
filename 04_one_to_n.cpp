dfed8510cc0c91647c93fce25f3eead6bb1f901a
a-lagopus <a-lagopus@yandex.ru>
2018-10-21 15:53:55 +0100

    Added program 4

--- 04_one_to_n.cpp
+++ 04_one_to_n.cpp
@@ -10,10 +10,14 @@ int main()
     int number,sum;
     cout << "Enter the number: ";
     cin >> number;
-    sum = 1;
+    if(number<1){
+	cout << "Error! Number must be positive" << "\n";
+	return 0;
+    }
+    sum = 0;
     for(int i=1;i<=number;i++){
 	sum+=i;
     }
-    cout << "Sum 1 to n is: " << sum << "\n";
+    cout << "Sum of numbers 1 to " << number << " is: " << sum << "\n";
     return 0;
 }
