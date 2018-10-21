45dbeeb3008224982a8728785f819720644e1eb1
a-lagopus <a-lagopus@yandex.ru>
2018-10-21 18:45:46 +0100

    How does this thing work???

--- 04_one_to_n.cpp
+++ 04_one_to_n.cpp
@@ -15,9 +15,7 @@ int main()
 	return 0;
     }
     sum = 0;
-    for(int i=1;i<=number;i++){
-	sum+=i;
-    }
+    for(int i=1;i<=number;i++)  sum+=i;
     cout << "Sum of numbers 1 to " << number << " is: " << sum << "\n";
     return 0;
 }
--- 05_one_to_n_mod.cpp
+++ 05_one_to_n_mod.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 
-//04 - A program that asks the user for a number n and prints the sum of the numbers 1 to n
+// A program that asks the user for a number n and prints the sum of the numbers 1 to n
+// 05 - Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
 
 using namespace std;
 
@@ -16,7 +17,7 @@ int main()
     }
     sum = 0;
     for(int i=1;i<=number;i++){
-	sum+=i;
+        if((i%3==0) || (i%5==0)) sum+=i;
     }
     cout << "Sum of numbers 1 to " << number << " is: " << sum << "\n";
     return 0;
